69b18820fa3fd1a44b4fe783c5dbdd562871e96b
Abdullah AL Habib <aalhabib001@gmail.com>
2021-02-15 22:31:27 +0600

    Extracted Some Widgets

--- lib/view/Regipage.dart
+++ lib/view/Regipage.dart
@@ -329,7 +329,7 @@ class _RegiPageState extends State<RegiPage> {
                                               }).toList(),
                                             ),
                                           )),
-                                    ],
+                                    ],//comment
                                   ),
                                 ),
                               )
