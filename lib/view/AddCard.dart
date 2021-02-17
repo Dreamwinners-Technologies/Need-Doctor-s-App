9a61faf75add91c97a1d73a54444a8af8f4daea9
Abdullah AL Habib <aalhabib001@gmail.com>
2021-02-17 21:04:14 +0600

    Merged Conflict

--- lib/view/AddCard.dart
+++ lib/view/AddCard.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:need_doctors/Animation/FadeAnimation.dart';
 import 'package:need_doctors/Colors/Colors.dart';
 import 'package:need_doctors/Widgets/ToastNotification.dart';
@@ -12,9 +12,10 @@ import 'package:need_doctors/items/objectdata.dart';
 import 'package:need_doctors/models/Card/AddCardRequest.dart';
 import 'package:need_doctors/models/MessageIdResponse.dart';
 import 'package:need_doctors/models/StaticData/DistrictListRaw.dart';
+import 'package:need_doctors/models/StaticData/DistrictLists.dart';
+import 'package:need_doctors/models/StaticData/ThanaListRaw.dart';
+import 'package:need_doctors/models/StaticData/ThanaLists.dart';
 import 'package:need_doctors/networking/CardNetwork.dart';
-
-import 'RegiPage.dart';
 
 class AddCardPage extends StatefulWidget {
   AddCardPage({Key key}) : super(key: key);
@@ -53,9 +54,14 @@ class _AddCardPageState extends State<AddCardPage> {
 
   //Image Picking:
   File _image;
+
   Future imagepick(ImageSource source) async {
     // ignore: deprecated_member_use
-    File image = await ImagePicker.pickImage(source: source, maxHeight: 600, maxWidth: 800);
+    File image = await ImagePicker.pickImage(
+      source: source,
+      maxHeight: 600,
+      maxWidth: 1000,
+    );
     if (image != null) {
       cropimage(image);
     }
@@ -68,9 +74,9 @@ class _AddCardPageState extends State<AddCardPage> {
             toolbarColor: primaryColor,
             toolbarTitle: 'Crope Image'),
         sourcePath: file.path,
-        maxHeight: 200,
-        maxWidth: 370,
-        aspectRatio: CropAspectRatio(ratioX: 1.0, ratioY: 1.0));
+        maxHeight: 600,
+        maxWidth: 1000,
+        aspectRatio: CropAspectRatio(ratioX: 10, ratioY: 6));
     if (cropped != null) {
       setState(() {
         _image = cropped;
@@ -85,207 +91,205 @@ class _AddCardPageState extends State<AddCardPage> {
         title: Text("Add Card"),
         backgroundColor: primaryColor,
       ),
-      body: Column(
-        children: <Widget>[
-          Stack(
-            children: [
-              Container(
-                margin: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.width * .05),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    border: Border.all(width: 1.0, color: Color(0xff008080))),
-                height: MediaQuery.of(context).size.height / 4,
-                width: MediaQuery.of(context).size.width * .9,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: _image != null
-                      ? Image.file(
-                          _image,
-                          fit: BoxFit.cover,
-                        )
-                      : Container(
-                          height: 200,
-                          width: 370,
-                        ),
-                ),
-              ),
-              Positioned(
-                  top: 50.0,
-                  left: 130.0,
-                  child: Container(
-                    height: 100.0,
-                    width: 100.0,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100.0),
-                        child: Visibility(
-                            visible: _image == null ? true : false,
-                            child: Image.asset("asset/images/Noimage.jpg"))),
-                  ))
-            ],
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 50.0,
-            margin: const EdgeInsets.only(
-                left: 12.0, right: 12.0, top: 12.0, bottom: 12.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Stack(
               children: [
-                //Camera
-                GestureDetector(
-                  onTap: () {
-                    imagefromcamera();
-                  },
-                  child: Container(
-                    height: 50.0,
-                    width: 50.0,
-                    margin: const EdgeInsets.only(right: 25.0),
-                    decoration: BoxDecoration(
-                        color: primaryColor, shape: BoxShape.circle),
-                    child: Center(
-                        child: Icon(
-                      Icons.add_a_photo,
-                      color: white,
-                      size: 26,
-                    )),
+                Container(
+                  margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.width * .05),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      border: Border.all(width: 1.0, color: Color(0xff008080))),
+                  height: MediaQuery.of(context).size.height / 4,
+                  width: MediaQuery.of(context).size.width * .9,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: _image != null
+                        ? Image.file(
+                            _image,
+                            fit: BoxFit.cover,
+                          )
+                        : Container(
+                            height: 200,
+                            width: 370,
+                          ),
                   ),
                 ),
-                //Gallary:
-                GestureDetector(
-                  onTap: () {
-                    imagefromgallary();
-                  },
-                  child: Container(
-                    height: 50.0, width: 50.0,
-                    // padding: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                        color: primaryColor, shape: BoxShape.circle),
-                    child: Center(
-                        child: Icon(
-                      Icons.photo_library,
-                      color: white,
-                      size: 25,
-                    )),
-                  ),
-                ),
+                Positioned(
+                    top: 50.0,
+                    left: 130.0,
+                    child: Container(
+                      height: 100.0,
+                      width: 100.0,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100.0),
+                          child: Visibility(
+                              visible: _image == null ? true : false,
+                              child: Image.asset("asset/images/Noimage.jpg"))),
+                    ))
               ],
             ),
-          ),
-          SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Align(
-                      alignment: FractionalOffset(0.1, 0.2),
-                      child: Text(
-                        'Check Info',
-                        style: TextStyle(
-                          color: Color(0xff008080),
-                          fontSize: 20,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 5, 20, 1),
-                  child: FadeAnimation(
-                    1,
-                    _buildTextField1(
-                      nameController,
-                      'Dr. Name',
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 50.0,
+              margin: const EdgeInsets.only(
+                  left: 12.0, right: 12.0, top: 12.0, bottom: 12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //Camera
+                  GestureDetector(
+                    onTap: () {
+                      imagepick(ImageSource.camera);
+                    },
+                    child: Container(
+                      height: 50.0,
+                      width: 50.0,
+                      margin: const EdgeInsets.only(right: 25.0),
+                      decoration: BoxDecoration(
+                          color: primaryColor, shape: BoxShape.circle),
+                      child: Center(
+                          child: Icon(
+                        Icons.add_a_photo,
+                        color: white,
+                        size: 26,
+                      )),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 5, 20, 1),
-                  child: FadeAnimation(
-                    1,
-                    // _buildTextField1(
-                    //   nameController,
-                    //   'Specialization',
-                    // ),
-                    specializationContainer(),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 5, 20, 1),
-                  child: FadeAnimation(
-                    1,
-                    // DistrctDropDown(),
-                    districtListDropDown(context),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 5, 20, 1),
-                  child: FadeAnimation(
-                    1,
-                    // ThanaDropDown(),
-                    thanaListDropDown(context),
-                  ),
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                FadeAnimation(
-                  1,
-                  MaterialButton(
-                    minWidth: 100,
-                    height: 35,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(24.0))),
-                    onPressed: () async {
-                      shape:
-                      // RoundedRectangleBorder(
-                      //     borderRadius:
-                      //         BorderRadius.all(Radius.circular(24.0)));
-
-                      if (nameController.text.isEmpty ||
-                          thanaController.text.isEmpty) {
-                        sendToast("Name or Thana Cant be empty");
-                        throw new Exception("Field Cant be empty");
-                      }
-
-                      AddCardRequest addCardRequest = AddCardRequest(
-                          appointmentNo: "",
-                          name: nameController.text,
-                          specialization: selectSpeciality,
-                          thana: thanaController.text,
-                          district: selectDis);
-
-                      MessageIdResponse response =
-                          await addCard(addCardRequest: addCardRequest);
-
-                      if (response != null) {
-                        int statusCode = await uploadFile(
-                            cardId: response.id, image: _image);
-
-                        print(statusCode);
-                        if (statusCode == 201) {
-                          setState(() {
-                            _image = null;
-                            nameController.clear();
-                            thanaController.clear();
-                          });
-                          _image.delete();
-                        }
-                      }
+                  //Gallary:
+                  GestureDetector(
+                    onTap: () {
+                      imagepick(ImageSource.gallery);
                     },
-                    color: Color(0xff008080),
-                    child: Text('Save',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold)),
+                    child: Container(
+                      height: 50.0, width: 50.0,
+                      // padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                          color: primaryColor, shape: BoxShape.circle),
+                      child: Center(
+                          child: Icon(
+                        Icons.photo_library,
+                        color: white,
+                        size: 25,
+                      )),
+                    ),
                   ),
-                ),
+                ],
+              ),
+            ),
+            Column(
+              children: <Widget>[
+                Align(
+                  alignment: FractionalOffset(0.1, 0.2),
+                  child: Text(
+                    'Check Info',
+                    style: TextStyle(
+                      color: Color(0xff008080),
+                      fontSize: 20,
+                    ),
+                  ),
+                )
               ],
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 5, 20, 1),
+              child: FadeAnimation(
+                1,
+                _buildTextField1(
+                  nameController,
+                  'Dr. Name',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 5, 20, 1),
+              child: FadeAnimation(
+                1,
+                // _buildTextField1(
+                //   nameController,
+                //   'Specialization',
+                // ),
+                specializationContainer(),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 5, 20, 1),
+              child: FadeAnimation(
+                1,
+                // DistrctDropDown(),
+                districtListDropDown(context),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 5, 20, 1),
+              child: FadeAnimation(
+                1,
+                // ThanaDropDown(),
+                thanaListDropDown(context),
+              ),
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            FadeAnimation(
+              1,
+              MaterialButton(
+                minWidth: 100,
+                height: 35,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(24.0))),
+                onPressed: () async {
+                  shape:
+                  // RoundedRectangleBorder(
+                  //     borderRadius:
+                  //         BorderRadius.all(Radius.circular(24.0)));
+
+                  if (nameController.text.isEmpty) {
+                    sendToast("Name or Thana Cant be empty");
+                    throw new Exception("Field Cant be empty");
+                  }
+
+                  AddCardRequest addCardRequest = AddCardRequest(
+                      appointmentNo: "",
+                      name: nameController.text,
+                      specialization: selectSpeciality,
+                      thana: _selectedThana,
+                      district: _selectedDistrict);
+
+                  MessageIdResponse response =
+                      await addCard(addCardRequest: addCardRequest);
+
+                  print(_image.path);
+                  print(response.message);
+                  if (response != null) {
+                    print(1);
+                    int statusCode =
+                        await uploadFile(cardId: response.id, image: _image);
+
+                    print(statusCode);
+                    if (statusCode == 201) {
+                      setState(() {
+                        _image = null;
+                        nameController.clear();
+                        thanaController.clear();
+                      });
+                      _image.delete();
+                    }
+                  }
+                },
+                color: Color(0xff008080),
+                child: Text('Save',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -297,7 +301,7 @@ class _AddCardPageState extends State<AddCardPage> {
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8.9),
           image:
-          DecorationImage(image: AssetImage(_image.path), fit: BoxFit.fill),
+              DecorationImage(image: AssetImage(_image.path), fit: BoxFit.fill),
         ),
       );
     } else {
@@ -410,7 +414,7 @@ class _AddCardPageState extends State<AddCardPage> {
         },
         value: this.selectSpeciality,
         items: specalizationlist.map(
-              (val) {
+          (val) {
             return DropdownMenuItem(
               value: val,
               child: Text(
@@ -424,6 +428,7 @@ class _AddCardPageState extends State<AddCardPage> {
     );
   }
 }
+
 _buildTextField1(TextEditingController controller, String labelText) {
   return Container(
     decoration: BoxDecoration(
