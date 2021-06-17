b3c9c4d72d220e1ed5f03eea91ae57bfd3c94e89
szshohan008 <szshohan008@gmail.com>
2021-06-17 11:18:09 +0600

    Profile Update

--- lib/view/Profile/utils/editButton.dart
+++ lib/view/Profile/utils/editButton.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:need_doctors/Colors/Colors.dart';
 import 'package:need_doctors/view/ProfileEdit/ProfileEdit.dart';
 
-
-
 editbutton(BuildContext context) {
   return new Center(
     child: new Container(
@@ -11,21 +9,21 @@ editbutton(BuildContext context) {
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         color: primaryLight,
-        ),
+      ),
       child: new Material(
         child: new InkWell(
           onTap: () {
             Navigator.push(context,
-                               MaterialPageRoute(builder: (context) => ProfileEdit()));
+                MaterialPageRoute(builder: (context) => ProfileEdit()));
           },
           child: new Container(
             width: 34.0,
             height: 34.0,
             child: Icon(Icons.edit),
-            ),
           ),
-        color: Colors.transparent,
         ),
+        color: Colors.transparent,
       ),
-    );
+    ),
+  );
 }
