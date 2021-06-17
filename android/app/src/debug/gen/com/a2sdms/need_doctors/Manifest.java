2fadf6826718e2ff70436db02fed5fa026c3fb37
szshohan008 <szshohan008@gmail.com>
2021-06-17 07:50:18 +0600

    RegistrationPage Update

---
+++ android/app/src/debug/gen/com/a2sdms/need_doctors/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.a2sdms.need_doctors;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
